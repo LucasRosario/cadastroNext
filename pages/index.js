6218c81f3b99f460ce971032366cd733237aeeba
Rosario Lucas <lucasrosario1@hotmail.com>
2023-04-08 23:25:33 -0300

    Telas frontend finalizadas

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,31 @@
+import { getCookie } from 'cookies-next'
+import { verify } from '../services/user'
+
+
 export default function Home() {
   return (
     <div>
       Pagina segura - Perfil do usuario
     </div>
   )
+}
+
+export const getServerSideProps = async ({req, res}) => {
+  try {
+    const token = getCookie('authorization', { req, res })
+    if (!token) throw new Error('Token Invalido!')
+
+    verify(token)
+    return {
+      props: {}
+    }
+  } catch {
+    return {
+      redirect: { 
+        permanent: false,
+        destination: '/login'
+      },
+      props: {}
+    }
+  }
 }
--- services/user.js
+++ services/user.js
@@ -1,11 +1,33 @@
+import jwt from 'jsonwebtoken'
+
 let users = []
+
+const SECRET = process.env.JWT_SECRET
+
+function createToken(user) {
+    return jwt.sign({ email: user.email, name: user.name}, SECRET)
+}
+
+function readToken(token) {
+    try {
+        return jwt.verify(token, SECRET)
+    } catch (err) {
+        throw new Error('Token invalido')
+    }
+}
+
+export function verify(token) {
+    return readToken(token)
+}
 
 export function cadastro(body) {
     const user = users.find(({ email}) => email === body.email)
     if (user) throw new Error('Usuario já cadastrado')
 
     users.push(body)
-    return body
+    
+    const token = createToken(body)
+    return token
 }
 
 export function login(body) {
@@ -13,5 +35,6 @@ export function login(body) {
     if (!user) throw new Error('Usuario nao encontrado')
     if (user.password !== body.password) throw new Error('Senha incorreta!')
 
-    return user
+    const token = createToken(user)
+    return token
 }
